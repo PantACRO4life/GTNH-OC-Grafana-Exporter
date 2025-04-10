bc4fd6f926d5c7948aa1b4b7808918a7e7bca949
PantACRO4life <6643071+PantACRO4life@users.noreply.github.com>
2025-04-10 03:37:23 -0400

    see the comparison exporter.lua

--- exporter.lua
+++ exporter.lua
@@ -178,18 +178,15 @@ local function exportAllMachines()
             local coords = machine.getCoordinates() or "Unknown"
             
             -- Get sensor information
-            local sensorInfo = machine.getSensorInformation() or "No sensor data"
+            --local sensorInfo = machine.getSensorInformation() or "No sensor data"
   
-            postString = postString .. config.multiblockMeasurement .. ",machine=" .. sanitize(name) .. ",owner=" .. sanitize(owner) .. ",coord=" .. sanitize(coords) .. ",sensor=" .. sanitize(sensorInfo) .. "\n"
-            
+        local postString = config.multiblockMeasurement .. 
+            " machine=" .. name .. ",owner=" .. owner .. ",coord=" .. coords --.. ",sensor=" .. sensorInfo
         end
-    end
-    
-    if #postString > 0 then
-        print("Sending to InfluxDB: " .. postString)
+        
         internet.request(config.dbURL .. config.multiblockDB, postString)()
     end
-    print("No data to send")
+    
 end
 
 local function exportCpus(interface)
