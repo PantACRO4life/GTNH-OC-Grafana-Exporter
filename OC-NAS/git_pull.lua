b4dc21cae80b99f27a10ba34ffcd628626ca6a88
PantACRO4life <6643071+PantACRO4life@users.noreply.github.com>
2025-04-13 20:07:10 -0400

    Create git_pull.lua

---
+++ OC-NAS/git_pull.lua
@@ -0,0 +1,27 @@
+local files = {
+    "exporter.lua",
+    "config_enabled_energy.lua",
+    "config_enabled_fluids.lua",
+    "config_enabled_items.lua",
+    "config_enabled_essentia.lua",
+    "config_enabled_multi.lua",
+    "config_enabled_cpus.lua"
+  }
+  
+  local baseUrl = "https://raw.githubusercontent.com/PantACRO4life/GTNH-OC-Grafana-Exporter/refs/heads/main/"
+  local basePath = "/home/"
+  
+  for _, file in ipairs(files) do
+    local fullUrl = baseUrl .. file
+    local localPath = basePath .. file
+    local cmd = 'wget -f "' .. fullUrl .. '" ' .. localPath
+    print("[GET] " .. file)
+    os.execute(cmd)
+  
+    -- Write the timestamp file
+    local tsFile = io.open(localPath .. ".ts", "w")
+    tsFile:write(os.time())
+    tsFile:close()
+    print("[TS] Updated timestamp for " .. file)
+  end
+  
